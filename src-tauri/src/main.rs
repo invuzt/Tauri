f6c10e9e2449aa8762758aaf77cd748c00833bcd
invuzt <invuzt@mail.com>
2026-03-17 00:07:55 +0700

    setup tauri project

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,7 +1,4 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    odfiz_app_lib::run();
 }
